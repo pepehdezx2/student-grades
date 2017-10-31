85c10ace55a4379e41e69d1f1c07c38b00850551
Jose Antonio <joseantonio@MacBook-Pro-de-Jose.local>
2017-10-30 22:54:03 -0600

    Initial Commit

---
+++ tarea7b/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tarea7b
+//
+//  Created by Jose Antonio on 30/10/17.
+//  Copyright © 2017 Jose Antonio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
